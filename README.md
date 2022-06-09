8240a6cf9cb1df253793b5527cda0f7198b1ee63
Sergey <likaman@mail.ru>
2022-06-09 23:03:28 +1100

    ra-props1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
 
-## GH-Pages
+## GH-Pages: ()
--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import './App.css';
-import React from 'react';
-import Stars from './components/Stars/Stars';
+import "./App.css";
+import React from "react";
+import Stars from "./components/Stars/Stars";
 
-function App() {
-  return (
-    <div className="App">
-      <Stars count={5} />
-      <Stars count={4} />
-      <Stars count={3} />
-      <Stars count={2} />
-      <Stars count={1} />
-      <Stars count={0} />
-      <Stars count={6} />
-      <Stars count={2.6} />
-    </div>
-  );
+function App () {
+
+    return (
+        <div className="App">
+            <Stars count={5} />
+
+            <Stars count={4} />
+
+            <Stars count={3} />
+
+            <Stars count={2} />
+
+            <Stars count={1} />
+
+            <Stars count={0} />
+
+            <Stars count={6} />
+
+            <Stars count={2.6} />
+        </div>
+    );
+
 }
 
 export default App;
